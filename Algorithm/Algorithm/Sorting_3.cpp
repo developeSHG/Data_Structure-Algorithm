2260b9e13b4c3da9221ca71a6b6d7a1515995208
developeSHG <developeSHG@gmail.com>
2023-08-14 15:33:08 +0900

    퀵 정렬

---
+++ Algorithm/Algorithm/Sorting_3.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+#include <thread>
+// 오늘의 주제 : 정렬
+
+// C# 자료구조/알고리즘
+// -> A* OpenList (PQ)
+// -> C# List = C++ vector
+
+// PQ O(logN)
+// Red-Black Tree O(logN)
+// Sorting ?
+
+
+// [5][1][3][7][9][2][4][6][8]
+// p
+//           high low		  
+
+int Partition(vector<int>& v, int left, int right)
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+
+	// O(N)
+	while (low <= high)
+	{
+		while (low <= right && pivot >= v[low])
+			low++;
+
+		while (high >= left + 1 && pivot <= v[high])
+			high--;
+
+		if (low < high)
+			swap(v[low], v[high]);
+	}
+
+	swap(v[left], v[high]);
+	return high;
+}
+
+// O(N^2) < 최악
+// O(NlogN) < 평균
+void QuickSort(vector<int>& v, int left, int right)
+{
+	if (left > right)
+		return;
+
+	int pivot = Partition(v, left, right);
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
+}
+
+int main()
+{
+	vector<int> v;
+
+	srand(time(0));
+
+	for (int i = 0; i < 50; i++)
+	{
+		int randValue = rand() % 100;
+		v.push_back(randValue);
+	}
+
+	//BubbleSort(v);
+	//SelectionSort(v);
+	//InsertionSort(v);
+	//HeapSort(v);
+	//MergeSort(v, 0, v.size() - 1);
+	QuickSort(v, 0, v.size() - 1);
+}
